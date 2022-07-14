00de013672cc9baadb797d4df60f6226d09616e6
Sobhan <72103946+Sobhan-M@users.noreply.github.com>
2022-07-14 14:15:30 -0400

    Removed unused command file.

--- sobhan-m/dndiscord@dev/functions/events/discord/command/r.js
+++
@@ -1,6 +0,0 @@
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-
-await lib.discord.channels['@0.0.6'].messages.create({
-  channel_id: context.params.event.channel_id,
-  content: `<@!${context.params.event.member.user.id}> just triggered the **r** command!`
-});
