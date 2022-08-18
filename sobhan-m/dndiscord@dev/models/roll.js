152a73f81a9e6fd83106212887d6f72fdf52a36d
Sobhan <72103946+Sobhan-M@users.noreply.github.com>
2022-08-18 13:02:49 -0400

    Implemented formatArgument And Its Tests

--- sobhan-m/dndiscord@dev/models/roll.js
+++ sobhan-m/dndiscord@dev/models/roll.js
@@ -1,8 +1,22 @@
 
 module.exports = class Roll {
 
-	constructor(startingCommand) {
-		this.startingCommand = startingCommand;
+	constructor(command, argument) {
+		this.command = command;
+		this.arg = argument;
+	}
+
+	static formatArgument(arg) {
+		let rolls = arg.split(/[\+-]+/).filter(roll => roll !== "");
+		let operators = arg.split(/[^\+-]+/).filter(op => op !== "");
+
+		// If "+" at the beginning is implicit.
+		if (operators.length == rolls.length - 1)
+		{
+			operators.unshift("+");
+		}
+
+		return {rolls: rolls, operators: operators};
 	}
 
 };
---
+++ sobhan-m/dndiscord@dev/test/roll.test.js
@@ -0,0 +1,34 @@
+const Roll = require('../models/roll');
+const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe("Argument Splitting Tests", () => {
+	it("should be [1], [+]", () => {
+		expect(Roll.formatArgument("1")).to.eql({
+			rolls: ["1"],
+			operators: ["+"]
+		});
+	});
+
+	it("should be [1], [-]", () => {
+		expect(Roll.formatArgument("-1")).to.eql({
+			rolls: ["1"],
+			operators: ["-"]
+		});
+	});
+
+	it("should be [1d20,3], [+,-]", () => {
+		expect(Roll.formatArgument("1d20-3")).to.eql({
+			rolls: ["1d20", "3"],
+			operators: ["+", "-"]
+		});
+	});
+
+	it("should be [1d20,3], [-,+]", () => {
+		expect(Roll.formatArgument("-1d20+3")).to.eql({
+			rolls: ["1d20", "3"],
+			operators: ["-", "+"]
+		});
+	});
+});
